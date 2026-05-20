25c479433e9d7059d42fa4453b2a5e80e399520f
edward0rtiz <edwardarmandoortiz@gmail.com>
2026-05-19 21:59:40 -0500

    fix deploy.sh line 24 update proper name of s3 bucket

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ cd terraform
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=${DEFAULT_AWS_REGION:-us-east-1}
 terraform init -input=false \
-  -backend-config="bucket=twin-terraform-state-${AWS_ACCOUNT_ID}" \
+  -backend-config="bucket=strategicai-terraform-state-${AWS_ACCOUNT_ID}" \
   -backend-config="key=${ENVIRONMENT}/terraform.tfstate" \
   -backend-config="region=${AWS_REGION}" \
   -backend-config="dynamodb_table=strategicai-terraform-locks" \
